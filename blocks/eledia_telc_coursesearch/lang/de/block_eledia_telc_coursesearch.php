42763c05d9b0e3cfc6e833e6a6929b2280510306
Immanuel Pasanec <ip@eledia.de>
2025-07-13 14:47:24 +0200

    Added german translation, change plugin display name.

---
+++ blocks/eledia_telc_coursesearch/lang/de/block_eledia_telc_coursesearch.php
@@ -0,0 +1,109 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Lang strings for the My overview block.
+ *
+ * @package    block_eledia_telc_coursesearch
+ * @copyright  Mark Nelson <markn@moodle.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['allincludinghidden'] = 'Alle (einschließlich ausgeblendeter)';
+$string['all'] = 'Alle';
+$string['addtofavourites'] = 'Kurs mit Stern markieren';
+$string['aria:addtofavourites'] = 'Stern für';
+$string['aria:allcoursesincludinghidden'] = 'Alle Kurse anzeigen';
+$string['aria:allcourses'] = 'Alle Kurse anzeigen, außer ausgeblendete';
+$string['aria:card'] = 'Zur Kartenansicht wechseln';
+$string['aria:controls'] = 'Steuerelemente der Kursübersicht';
+$string['aria:courseactions'] = 'Aktionen für den Kurs';
+$string['aria:coursesummary'] = 'Kurzzusammenfassung des Kurses:';
+$string['aria:courseprogress'] = 'Kursfortschritt:';
+$string['aria:customfield'] = '{$a} benutzerdefiniertes Feld anzeigen';
+$string['aria:displaydropdown'] = 'Dropdown-Menü anzeigen';
+$string['aria:favourites'] = 'Nur markierte Kurse anzeigen';
+$string['aria:future'] = 'Zukünftige Kurse anzeigen';
+$string['aria:groupingdropdown'] = 'Gruppierungs-Dropdown-Menü';
+$string['aria:inprogress'] = 'Laufende Kurse anzeigen';
+$string['aria:list'] = 'Zur Listenansicht wechseln';
+$string['aria:past'] = 'Vergangene Kurse anzeigen';
+$string['aria:removefromfavourites'] = 'Stern entfernen für';
+$string['aria:summary'] = 'Zur Zusammenfassungsansicht wechseln';
+$string['aria:sortingdropdown'] = 'Sortier-Dropdown-Menü';
+$string['availablegroupings'] = 'Verfügbare Filter';
+$string['availablegroupings_desc'] = 'Kursfilter, die zur Auswahl durch Benutzer verfügbar sind. Wenn keine ausgewählt sind, werden alle Kurse angezeigt.';
+$string['card'] = 'Karte';
+$string['cards'] = 'Karten';
+$string['clearcatsearch'] = '[data-action="clearcatsearch"]';
+$string['courseprogress'] = 'Kursfortschritt:';
+$string['completepercent'] = '{$a}% abgeschlossen';
+$string['createcourse'] = 'Kurs erstellen';
+$string['customfield'] = 'Benutzerdefiniertes Feld';
+$string['customfiltergrouping'] = 'Zu verwendendes Feld';
+$string['customfiltergrouping_nofields'] = 'Diese Option erfordert ein benutzerdefiniertes Kursfeld, das für alle sichtbar ist.';
+$string['displaycategories'] = 'Kategorien anzeigen';
+$string['displaycategories_help'] = 'Zeigt die Kurskategorie auf Dashboard-Elementen wie Karten, Listen- und Zusammenfassungselementen an.';
+$string['favourites'] = 'Markierte';
+$string['future'] = 'Zukünftig';
+$string['inprogress'] = 'Laufend';
+$string['lastaccessed'] = 'Zuletzt aufgerufen';
+$string['layouts'] = 'Verfügbare Layouts';
+$string['layouts_help'] = 'Layoutoptionen für die Kursübersicht, die von Benutzern ausgewählt werden können. Wenn keine ausgewählt sind, wird das Kartenlayout verwendet.';
+$string['list'] = 'Liste';
+$string['eledia_telc_coursesearch:myaddinstance'] = 'Neuen Kursübersichtsblock zum Dashboard hinzufügen';
+$string['nocustomvalue'] = 'Kein(e) {$a}';
+$string['past'] = 'Vergangen';
+$string['pluginname'] = 'Kurssuche';
+$string['privacy:metadata:overviewsortpreference'] = 'Sortierpräferenz für den Kursübersichtsblock.';
+$string['privacy:metadata:overviewviewpreference'] = 'Anzeigepräferenz für den Kursübersichtsblock.';
+$string['privacy:metadata:overviewgroupingpreference'] = 'Gruppierungspräferenz für den Kursübersichtsblock.';
+$string['privacy:metadata:overviewpagingpreference'] = 'Seiteneinstellungspräferenz für den Kursübersichtsblock.';
+$string['removefromfavourites'] = 'Stern für diesen Kurs entfernen';
+$string['searchcourses'] = "Kurse suchen";
+$string['searchcategories'] = "Kategorien suchen";
+$string['searchtags'] = "Tags suchen";
+$string['shortname'] = 'Kurzname';
+$string['summary'] = 'Zusammenfassung';
+$string['title'] = 'Kursname';
+$string['aria:hidecourse'] = '{$a} ausblenden';
+$string['aria:showcourse'] = '{$a} wieder anzeigen';
+$string['aria:hiddencourses'] = 'Ausgeblendete Kurse anzeigen';
+$string['hidden'] = 'Ausgeblendete Kurse';
+$string['hidecourse'] = 'Ausblenden';
+$string['hiddencourses'] = 'Ausgeblendet';
+$string['removeallitems'] = 'Alle entfernen';
+$string['noitemsavailable'] = 'Keine Elemente verfügbar';
+$string['noitemsselected'] = 'Keine Elemente ausgewählt';
+$string['show'] = 'Wieder anzeigen';
+$string['sortbytitle'] = 'Nach Kursname sortieren';
+$string['sortbylastaccessed'] = 'Nach letztem Zugriff sortieren';
+$string['sortbyshortname'] = 'Nach Kurzname sortieren';
+$string['privacy:request:preference:set'] = 'Der Wert der Einstellung \'{$a->name}\' war \'{$a->value}\'';
+$string['viewquickstart'] = 'Schnellstart-Anleitung anzeigen';
+$string['zero_default_title'] = 'Du bist in keinen Kurs eingeschrieben.';
+$string['zero_default_intro'] = 'Sobald du dich für einen Kurs anmeldest, wird er hier angezeigt.';
+$string['zero_nocourses_intro'] = 'Brauchen Sie Hilfe beim Einstieg? Lesen Sie die <a href="{$a->dochref}" title="{$a->doctitle}" target="{$a->doctarget}">Moodle-Dokumentation</a>.';
+$string['zero_nocourses_title'] = 'Erstelle deinen ersten Kurs';
+$string['zero_request_title'] = 'Fordere deinen ersten Kurs an';
+$string['zero_request_intro'] = 'Brauchen Sie Hilfe beim Einstieg? Lesen Sie die <a href="{$a->dochref}" title="{$a->doctitle}" target="{$a->doctarget}">Moodle-Dokumentation</a> oder starte mit unserem <a href="{$a->quickhref}" title="{$a->quicktitle}" target="{$a->quicktarget}">Schnellstart-Leitfaden</a>.';
+$string['nocatselected'] = 'Keine Kategorien ausgewählt';
+$string['nocatavailable'] = 'Keine Kategorien verfügbar';
+$string['notagsselected'] = 'Keine Tags ausgewählt';
+$string['notagsavailable'] = 'Keine Tags verfügbar';
+$string['nocustomfieldsselected'] = 'Keine benutzerdefinierten Felder ausgewählt';
+$string['nocustomfieldsavailable'] = 'Keine benutzerdefinierten Felder verfügbar';
+
